85e35e28526aa5763d25133aa1ecc38c4fae45a6
HugoGarrigues <hugo.garrigues@ynov.com>
2023-05-03 12:38:07 +0200

    Update serveur app.js

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const app = express();
-const vetements = require('shop-stussy.json');
+const port = 3000;
+const data = require('./data.json');
 
-app.use(express.static('public'));
+app.use(express.static('frontend'));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/html/index.html');
+app.listen(port, () => {
+  console.log('Serveur en écoute sur le port ${port}');
 });
 
-app.listen(3000, () => {
-  console.log('Serveur en écoute sur le port 3000');
+app.get('/sneaker/:id', (req, res) => {
+  const id = req.params.id;
+  const sneakers = data.sneakers;
+
+  const sneaker = sneakers.find((sneaker) => {
+    return sneaker.id == id;
+  }
+  );
+  res.json(sneaker);
+});
+
+app.get('/sneakers', (req, res) => {
+  const sneakers = data.sneakers;
+  res.json(sneakers);
+}
+
+);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/frontend/index.html');
+});
+
+app.use((req, res, next) => {
+  res.status(404).send("Désolé, cette page n'existe pas.");
 });
