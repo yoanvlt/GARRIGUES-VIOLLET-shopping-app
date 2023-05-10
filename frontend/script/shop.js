67aa94f32db0e8fad872ad6465ae26aada0a43da
yoanvlt <113009422+yoanvlt@users.noreply.github.com>
2023-05-10 09:02:01 +0200

    filtre

--- frontend/script/shop.js
+++ frontend/script/shop.js
@@ -1,40 +1,93 @@
+let activeFilters = [];
+let allOptions = ['tee', 'longsleeve', 'hoodie', 'crewneck', 'jeans', 'shorts'];
+
 function Display(option) {
+  let allCheckbox = document.getElementById("all");
+
   if (option == 1) {
+    activeFilters = [];
+    allCheckbox.checked = true;
+    allOptions.forEach(opt => document.getElementById(opt).checked = false);
     displayAllItems();
-}
-  else if (option == "tee") {
-    displayItems(option);
-}
-  else if (option == "longsleeve") {
-    displayItems(option);
-}
-  else if (option == "hoodie") {
-    displayItems(option);
-}
-  else if (option == "crewneck") {
-    displayItems(option);
-}
-  else if (option == "jeans") {
-    displayItems(option);
-}
-  else if (option == "shorts") {
-    displayItems(option);
-}
+  } else {
+    let index = activeFilters.indexOf(option);
+    let optionCheckbox = document.getElementById(option);
+
+    if (index > -1) {
+      activeFilters.splice(index, 1);
+      optionCheckbox.checked = false;
+    } else {
+      activeFilters.push(option);
+      optionCheckbox.checked = true;
+    }
+    if (activeFilters.length === 0) {
+      allCheckbox.checked = true;
+    } else {
+      allCheckbox.checked = false;
+    }
+
+    displayFilteredItems();
+  }
 }
 
-function displayAllItems() {
+
+function displayFilteredItems() {
   var container = document.getElementById("container");
+  container.innerHTML = "";
 
   fetch("http://localhost:3000/stussy")
     .then((response) => response.json())
     .then((data) => {
-      container.innerHTML = "";
+      data.forEach((item) => {
+        if (activeFilters.includes(item.class)) {
+          var div = document.createElement("div");
+          div.className = "item";
+
+          var img = document.createElement("img");
+          img.src = item.img_1;
+          img.alt = item.name;
+
+          img.addEventListener("mouseover", function() {
+            img.src = item.img_2;
+          });
+
+          img.addEventListener("mouseout", function() {
+            img.src = item.img_1;
+          });
+
+          img.addEventListener("click", function() {
+            window.location.href = "http://localhost:3000/stussy/" + item.id;
+          });
+
+          var h2 = document.createElement("h2");
+          h2.textContent = item.name;
+
+          var p = document.createElement("p");
+          p.textContent = item.price + " €";
+
+          div.appendChild(img);
+          div.appendChild(h2);
+          div.appendChild(p);
+
+          container.appendChild(div);
+        }
+      });
+    });
+}
+
+function displayAllItems() {
+  var container = document.getElementById("container");
+  container.innerHTML = "";
+
+  fetch("http://localhost:3000/stussy")
+    .then((response) => response.json())
+    .then((data) => {
       data.forEach((item) => {
         var div = document.createElement("div");
         div.className = "item";
 
         var img = document.createElement("img");
-        img.src = item.img_1; 
+        img.src = item.img_1;
         img.alt = item.name;
 
         img.addEventListener("mouseover", function() {
@@ -64,55 +117,7 @@ function displayAllItems() {
     });
 }
 
-            
-function displayItems(className) {
-  var container = document.getElementById("container");
-  container.innerHTML = ""; 
-
-  fetch("http://localhost:3000/stussy")
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((item) => {
-        if (item.class === className) {
-          var div = document.createElement("div");
-          div.className = "item";
-  
-          var img = document.createElement("img");
-          img.src = item.img_1;
-          img.alt = item.name;
-  
-          img.addEventListener("mouseover", function() {
-            img.src = item.img_2;
-          });
-  
-          img.addEventListener("mouseout", function() {
-            img.src = item.img_1;
-          });
-
-          img.addEventListener("click", function() {
-            window.location.href = "http://localhost:3000/stussy/" + item.id;
-          });
-  
-          var h2 = document.createElement("h2");
-          h2.textContent = item.name;
-  
-          var p = document.createElement("p");
-          p.textContent = item.price + " €";
-  
-          div.appendChild(img);
-          div.appendChild(h2);
-          div.appendChild(p);
-  
-          container.appendChild(div);
-        }
-      });
-    });
-}
-
 
 window.onload = function() {
   displayAllItems();
 };
-
-
-
